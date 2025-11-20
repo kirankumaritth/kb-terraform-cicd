92ab886ddbc357a9fbadb8f0fc32eedac37da510
Janjirala Kiran Kumar <kiran.itth@gmail.com>
2025-11-20 08:42:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
+resource "random_id" "suffix" {
+  byte_length = 2
+}
+
 resource "aws_s3_bucket" "demo" {
   bucket = "kb-demo-${terraform.workspace}-${random_id.suffix.hex}"
 
   tags = {
     Project = "KloudBuild"
-    Env     = terraform.workspace
+    Environment = terraform.workspace
   }
-}
-
-resource "random_id" "suffix" {
-  byte_length = 2
 }
